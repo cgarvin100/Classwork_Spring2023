79d8c79734ece16698559109dc30743a033783bf
cgarvin100 <92275950+cgarvin100@users.noreply.github.com>
2023-01-27 12:55:20 -0500

    modulated dosing.py file

--- dosing.py
+++ dosing.py
@@ -5,8 +5,7 @@
              type of medical treatment or diagnostics.
 """
 
-
-def dose_amount():
+def get_diagnosis():
     print("Day One Dosing Guidelines")
     print("")
     print("Choose diagnosis:")
@@ -14,25 +13,42 @@ def dose_amount():
     print("2 - Acute bacterial sinusitis")
     print("3 - Community-acquired pneumonia")
     print("4 - Pharyngitis/tonsilitis")
+
     diagnosis = int(input("Enter a number: "))
+    return diagnosis
+
+def get_patient_weight():
     print("PATIENT WEIGHT")
     print("Enter patient weight followed by units of kg or lb.")
     print("Examples:  65.3 lb      21.0 kg")
     weight_input = input("Enter weight: ")
+    return weight_input
+
+def parse_weight(weight_input):
     weight_data = weight_input.split(" ")
     weight = float(weight_data[0])
     units = weight_data[1]
     if units == "lb":
         weight = weight / 2.205
+    
+    return weight
+
+def compute_dosage(diagnosis, weight):
     dosages_mg_per_kg = [30, 10, 10, 12]
     dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
+    
+    return dosage_mg_first_day
+
+def output_results(weight, dosage_mg_first_day):
     print("CORRECT DOSAGE")
     print("For a patient weighing {:.1f} kg,".format(weight))
     print("  the correct dosage is {:.1f} mg the first day"
           .format(dosage_mg_first_day))
-
+    return
 
 if __name__ == '__main__':
-    dose_amount()
-
+    diagnosis = get_diagnosis()
+    weight = parse_weight(get_patient_weight())
+    dosage_mg_first_day = compute_dosage(diagnosis, weight)
+    output_results(weight, dosage_mg_first_day)
