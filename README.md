1689eb257f704a8a634f9d863d1b8ec5c2687ec7
cgarvin100 <92275950+cgarvin100@users.noreply.github.com>
2023-02-01 13:11:53 -0500

    added my own function and readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# Patient Health Database
+
+Author:  David Ward
+
+## Introduction
+This repository contains code for running a Patient Health Database.
+
+This is written in Python.
+
+## Specifications
+The database will contain:
+* Patient Name
+* Patient Age
+* Patient Bloodtype
+  - A+
+  - A-
+  - B+
+  - B-
+  - AB+
+  - AB-
+
+## Future additions
+1. Patient address
+1. Patient Gender
+1. Medical Record Number
--- databases.py
+++ databases.py
@@ -1,6 +1,48 @@
 def create_patient_entry(patient_name, patient_mrn, patient_age):
-    new_patient = [patient_name, patient_mrn, patient_age]
+    new_patient = [patient_name, patient_mrn, patient_age, []]
     return new_patient
+
+def get_patient_entry(db, mrn):
+    for patient in db:
+        if patient[1] == mrn:
+            return patient
+    return False
+
+def add_test_to_patient(db, mrn, test_name, test_value):
+    patient = get_patient_entry(db, mrn)
+    if patient is False:
+        print("Patient does not exist")
+        return
+    patient[3].append([test_name, test_value])
+    print(db)
+    return
+
+def print_directory(db, room_numbers):
+    for i,patient in enumerate(db):
+        print("Patient {} is in room {}".format(patient[0], room_numbers[i]))
+
+    print("\n----------Method 2----------\n")
+
+    for patient,number in zip(db, room_numbers):
+        print("Patient {} is in room {}".format(patient[0], number))
+    return
+
+def get_patient_test_result(db, mrn, test_name):
+    patient = get_patient_entry(db, mrn)
+    if patient == False:
+        print("Patient does not exist")
+        return False
+    
+    pateint_tests = patient[3]
+    return find_result(pateint_tests, test_name)
+
+
+def find_result(tests, test_name):
+    for test in tests:
+        if test_name == test[0]:
+            return test[1]
+    else:
+        return False
 
 
 def main_driver():
@@ -8,15 +50,21 @@ def main_driver():
     db.append(create_patient_entry("Anne Ables", 1, 34))
     db.append(create_patient_entry("Bob Boyles", 2, 45))
     db.append(create_patient_entry("Chris Chou", 3, 63))
+
     print(db)
     print("Bob's Age: " + str(db[1][2]))
-    print("PAtirn MRN 1: " + str(get_patient_entry(db, 1)))
+    print("Patient MRN 1: " + str(get_patient_entry(db, 1)))
+    add_test_to_patient(db, 1, "HDL", 120)
+    print(db)
 
-def get_patient_entry(db, mrn):
-    for patient in db:
-        if patient[1] == mrn:
-            return patient
-    return None
+    room_numbers = ["103", "232", "333"]
+
+    print_directory(db, room_numbers)
+
+    HDL_test_result  = get_patient_test_result(db, 1, 'HDL')
+    print(HDL_test_result)
+
+    return
 
 if __name__ == '__main__':
     main_driver()
